2f843397e0883a8caf4c8772bec4baa01c166ae6
Dr.Peachy <141729265+peachymorse@users.noreply.github.com>
2025-03-31 15:55:10 +0100

    styling for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,23 @@
-import './Hero.css';
+import { Link } from 'react-router-dom';
+import  './Hero.css'
 
 function Hero() {
   return (
-    <section className="hero">
-      <div className="hero-text">
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          We are a family-owned Mediterranean restaurant, focused on traditional
-          recipes served with a modern twist.
-        </p>
-        <button>Reserve a Table</button>
-      </div>
-      <div className="hero-image">
-        <img src="./assets/restaurantfood.jpg" alt="Restaurant food" />
+    <section className="hero-section">
+      <div className="hero-container">
+        <div className="hero-text">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          </p>
+          <Link to="/booking">
+            <button>Reserve a Table</button>
+          </Link>
+        </div>
+        <div className="hero-image">
+          <img src="/assets/bruchetta.svg" alt="Bruschetta" loading="lazy" />
+        </div>
       </div>
     </section>
   );
